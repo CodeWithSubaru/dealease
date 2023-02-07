7c9978de73ef58a4865794321e8290a773cfba83
CodeWithSubaru <francis.cloydm@gmail.com>
2023-02-07 16:51:26 +0800

    User CRUD operation (unfinished)

--- resources/js/pages/ChangePassword.vue
+++ resources/js/pages/ChangePassword.vue
@@ -2,7 +2,13 @@
     <Settings>
         <h2>Profile</h2>
 
-        <Table></Table>
+        <label for="">Old Password</label>
+        <input type="text" id="" name="" /><br /><br />
+        <label for="">New Password</label>
+        <input type="text" id="" name="" /><br /><br />
+        <label for="">Confirm Password</label>
+        <input type="text" id="" name="" /><br /><br />
+        <input type="submit" value="Submit" />
     </Settings>
 </template>
 
